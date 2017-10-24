cb289a7ceffaa0886c6d38de4a8734bdd6c9a3a5
rhidayat122 <151610243@smkassalaambandung.sch.id>
2017-10-24 08:47:39 +0400

    latihanya

---
+++ index4.php
@@ -0,0 +1,13 @@
+<?php
+
+	class ayam{
+
+		public $kaki = 2;
+		public $jenis = 'unggas';
+
+	}
+	$ayam1 = new ayam;
+	echo "Ayam berjalan dengan :".$ayam1-> kaki .'<br>' ;
+	echo "dan hewan berjenis :".$ayam1-> jenis .'<br>' ;
+
+?>
